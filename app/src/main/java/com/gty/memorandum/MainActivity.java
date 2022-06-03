17456181cd9d0af6026b92080efd75a4a92515b6
bulang <884596371@qq.com>
2022-06-03 08:46:42 +0800

    init

---
+++ app/src/main/java/com/gty/memorandum/MainActivity.java
@@ -0,0 +1,14 @@
+package com.gty.memorandum;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
